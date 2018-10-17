277159269707120ff5128144146a850fbd00422d
kiezmor <kiezmor60@gmail.com>
2018-10-17 16:59:06 +0200

    .gitignore & cmd help

---
+++ commands/help.js
@@ -0,0 +1,23 @@
+exports.run = async (bot, message, args) => {
+    var str = "";
+    bot.commands.forEach(function(name){
+        str += name.help.name + '\n';
+    });
+    message.channel.send({embed: {
+        color: 3447003,
+        fields: [{
+            name: "List of commands",
+            value: str
+          },
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: bot.user.avatarURL,
+          text: "Oppai BOT"
+        }
+      }
+    });
+}  
+exports.help = {
+    name: 'help'
+}
